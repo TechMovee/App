882addcfafffdd0a3d6916c1553bc933771ce14a
Beatriz Belaparte Favero <beatrizfavero-ieg@instituto.ad>
2024-09-23 10:15:15 -0300

    colocando foto de perfil no cadastro

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,15 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
+
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }  // Adicione o repositório JitPack aqui
+    }
 }
+
 
 rootProject.name = "TechMovee"
 include(":app")
